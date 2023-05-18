f0d390f3ac51ec1d12e6e84df741183ae87e60ab
Cesar Pablo Garcia Suarez <cesarpgasuz@gmail.com>
2023-05-17 18:54:02 -0600

    deployment

---
+++ routes/veterinarioRoutes.js
@@ -0,0 +1,27 @@
+import express from "express";
+import {registrar, perfil, confirmar, autenticar, olvidePassword, comprobarToken, nuevoPassword, actualizarPerfil, actualizarPassword} from '../controllers/veterinarioController.js';
+import checkAuth from "../middleware/authMiddleware.js";
+
+const router = express.Router();
+
+// ara publica
+router.post('/', registrar);
+router.get('/confirmar/:token', confirmar);
+router.post('/login', autenticar);
+router.post('/olvide-password', olvidePassword);
+
+
+// router.get('/olvide-password/:token', comprobarToken);
+// router.post('/olvide-password/:token', nuevoPassword);
+// es lo mismo que: 
+router.route('/olvide-password/:token').get(comprobarToken).post(nuevoPassword);
+
+
+//area privada
+router.get('/perfil', checkAuth, perfil);
+router.put('/perfil/:id', checkAuth, actualizarPerfil)
+router.put('/actualizar-password', checkAuth, actualizarPassword)
+
+
+
+export default router; 
